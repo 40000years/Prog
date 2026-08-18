7d4f05dc05a0b6578a409bd51529d0fa9968209d
40000years <thanaphatyamboo@gmail.com>
2026-08-18 14:22:30 +0700

    🔧 Revert: Lock availability_zone to ap-southeast-7a as requested by user

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,6 +44,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidr
+  availability_zone       = "${var.aws_region}a"
   map_public_ip_on_launch = true
 
   tags = {
