c1b38839ee57756dc1276fd7cd923c70701cd75f
40000years <thanaphatyamboo@gmail.com>
2026-08-19 14:25:51 +0700

    🔧 Fix Terraform syntax: Change unsupported 'action' block to standard 'resource "ansible_playbook"'

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -1,35 +1,18 @@
 # ============================================================
-# Ansible Action Configuration (Terraform Action)
+# Ansible Playbook Resource
 # ============================================================
 
-action "ansible_playbook_run" "ansible" {
-  config {
-    playbooks = ["${path.module}/../ansible/playbook.yml"]
+resource "ansible_playbook" "app_deployment" {
+  playbook = "${path.module}/../ansible/playbook.yml"
+  name     = aws_instance.web.public_ip
 
-    inventories = [
-      jsonencode({
-        tag_Role_web = {
-          hosts = [aws_instance.web.public_ip]
-          vars = {
-            ansible_user                 = "ubuntu"
-            ansible_ssh_private_key_file = "/tmp/id_rsa"
-            ansible_ssh_common_args      = "-o StrictHostKeyChecking=no"
-          }
-        }
-        tag_Role_db = {
-          hosts = [aws_instance.db.public_ip]
-          vars = {
-            ansible_user                 = "ubuntu"
-            ansible_ssh_private_key_file = "/tmp/id_rsa"
-            ansible_ssh_common_args      = "-o StrictHostKeyChecking=no"
-          }
-        }
-      })
-    ]
-
-    extra_vars = {
-      db_host     = aws_instance.db.private_ip
-      db_password = var.db_password
-    }
+  extra_vars = {
+    db_host     = aws_instance.db.private_ip
+    db_password = var.db_password
   }
+
+  depends_on = [
+    aws_instance.web,
+    aws_instance.db
+  ]
 }
