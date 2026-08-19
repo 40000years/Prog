f99ef36ed23bc0323d9a09a7be065238f5b33483
40000years <thanaphatyamboo@gmail.com>
2026-08-19 14:27:51 +0700

    🚀 Enable Terraform Action block: Upgrade terraform_version to latest and configure action "ansible_playbook_run"

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -1,18 +1,35 @@
 # ============================================================
-# Ansible Playbook Resource
+# Ansible Action Configuration (Terraform Action Block)
 # ============================================================
 
-resource "ansible_playbook" "app_deployment" {
-  playbook = "${path.module}/../ansible/playbook.yml"
-  name     = aws_instance.web.public_ip
+action "ansible_playbook_run" "ansible" {
+  config {
+    playbooks = ["${path.module}/../ansible/playbook.yml"]
 
-  extra_vars = {
-    db_host     = aws_instance.db.private_ip
-    db_password = var.db_password
+    inventories = [
+      jsonencode({
+        tag_Role_web = {
+          hosts = [aws_instance.web.public_ip]
+          vars = {
+            ansible_user                 = "ubuntu"
+            ansible_ssh_private_key_file = "/tmp/id_rsa"
+            ansible_ssh_common_args      = "-o StrictHostKeyChecking=no"
+          }
+        }
+        tag_Role_db = {
+          hosts = [aws_instance.db.public_ip]
+          vars = {
+            ansible_user                 = "ubuntu"
+            ansible_ssh_private_key_file = "/tmp/id_rsa"
+            ansible_ssh_common_args      = "-o StrictHostKeyChecking=no"
+          }
+        }
+      })
+    ]
+
+    extra_vars = {
+      db_host     = aws_instance.db.private_ip
+      db_password = var.db_password
+    }
   }
-
-  depends_on = [
-    aws_instance.web,
-    aws_instance.db
-  ]
 }
